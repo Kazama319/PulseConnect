8ae8c6cdcd8e9cb44cba7eb3b6fc7f5b3be78c67
Kazama319 <you@example.combhanupratap_co21a3_40@dtu.ac.in>
2024-08-02 19:38:51 +0530

    server url updated

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://pulse-connect.onrender.com/",
     // credentials: true,
   },
 });
